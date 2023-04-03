d4756eb370983e15b526eefc2d9be1b8f89128e2
Martin <helma.martin@accenture.com>
2023-04-03 17:44:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/Myspringproject1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspringproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myspringproject1Application.class, args);
+		System.out.println("testing");
+	}
+
+}
